264c3807c3e36913c6fd9f502afbf8ed1564a008
pluxery <84563687+pluxery@users.noreply.github.com>
2022-02-01 21:02:54 +1000

    #

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const cors = require("cors");
 const express = require('express')
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 const app = express()
 
@@ -12,7 +13,7 @@ app.use('/sign', require('./Routers/authRouter'))
 app.use('/post', require('./Routers/postRouter'))
 app.use('/retweet', require('./Routers/retweetRouter'))
 
-const PORT = 5000
+const PORT = 5000 || process.env.PORT
 const URI ='mongodb+srv://sema:123@cluster0.2hlcl.mongodb.net/Twitter?retryWrites=true&w=majority'
 
 
@@ -23,7 +24,7 @@ async function start() {
             useUnifiedTopology: true,
             useCreateIndex: true
         }
-        app.listen(process.env.PORT, () => console.log(`App has been started on port ${PORT}...`))
+        app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
     } catch (e) {
         console.log('Server Error', e.message)
         process.exit(1)
