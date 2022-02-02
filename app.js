1541b4f6dbc2d27a3b6a0cf8d3a049323d79b970
pluxery <84563687+pluxery@users.noreply.github.com>
2022-02-02 16:25:58 +1000

    make it better

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const cors = require("cors");
 const express = require('express')
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 const app = express()
-
-app.use(cors());
-
 app.use(express.json({extended: true}))
 
 
@@ -13,17 +11,19 @@ app.use('/sign', require('./routers/authRouter'))
 app.use('/post', require('./routers/postRouter'))
 app.use('/retweet', require('./routers/retweetRouter'))
 
-const PORT = 5000 || process.env.PORT
+const PORT = process.env.PORT || 5000
 const uri ='mongodb+srv://sema:123@cluster0.2hlcl.mongodb.net/Twitter2?retryWrites=true&w=majority'
+app.use(cors());
+
 
 
 async function start() {
     try {
-        await mongoose.connect(uri), {
+        await mongoose.connect(uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true
-        }
+        })
         app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
     } catch (e) {
         console.log('Server Error', e.message)
